4be9e111ebcefd65d7c58e5ec1d48f5f18b46120
msoehl <33597350+msoehl@users.noreply.github.com>
2022-12-26 22:51:52 +0100

    Adding comments

--- script.php
+++ script.php
@@ -1,9 +1,11 @@
 #!/usr/bin/php -q
 <?php
-chdir("/home/webcamed/public_html/webcam/31");
+// Your directory chdir("")
+chdir("");
 if ($handle = opendir('.')) {
 while (false !== ($file = readdir($handle))) {
 if ($file != "." && $file != ".." && preg_match("/jpg$/",$file) && $file !="webcam.jpg") {
+// Set the new file name
 rename($file, 'webcam.jpg');
 }
 }
